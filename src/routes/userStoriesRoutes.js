2a80730ef2e46c19efbfa6b8023c6d7df5164755
Kody <157696416+kydoCode@users.noreply.github.com>
2024-12-06 12:35:19 +0100

    Login and create user operationals

--- src/routes/userStoriesRoutes.js
+++ src/routes/userStoriesRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const userStoriesController = require('../controllers/userStoriesController');
-
 const router = express.Router();
+const userStoriesController = require('../controllers/userStoriesController');
+const authMiddleware = require('../middlewares/auth');
 
 // Routes CRUD
-router.post('/', userStoriesController.createUserStory); // Créer une user story
 router.get('/', userStoriesController.getUserStories); // Obtenir toutes les user stories
 router.get('/:id', userStoriesController.getUserStoryById); // Obtenir une user story par ID
+
+router.use(authMiddleware);
+router.post('/', userStoriesController.createUserStory); // Créer une user story
 router.put('/:id', userStoriesController.updateUserStory); // Mettre à jour une user story
 router.delete('/:id', userStoriesController.deleteUserStory); // Supprimer une user story
 
