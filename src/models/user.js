8ae2fc48e56d324cc3e143fec847c6fee88d7538
Kody <kydokody@gmail.com>
2025-01-25 19:56:48 +0100

    Push last lucid and logic to fecht role

--- src/models/user.js
+++ src/models/user.js
@@ -1,59 +1,61 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 const bcrypt = require('bcryptjs');
-const UserStory = require('./userStory'); // Import UserStory model
-const UserUserStory = require('./UserUserStory'); // Import UserUserStory model
 
-const User = sequelize.define('User', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  name: {
+const UserDefinition = (sequelize) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      comment: 'Name of the user',
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      comment: 'Unique email address of the user',
+      validate: {
+        isEmail: true,
+      }
+    },
+  password: {
     type: DataTypes.STRING,
     allowNull: false,
-    comment: 'Name of the user',
+    comment: 'Password of the user',
   },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    comment: 'Unique email address of the user',
-    validate: {
-      isEmail: true,
-    }
-  },
-password: {
-  type: DataTypes.STRING,
-  allowNull: false,
-  comment: 'Password of the user',
-},
 
-  role: {
-    type: DataTypes.ENUM('developer', 'product owner', 'tester', 'teammate', 'scrum master', 'administrator'),
-    allowNull: false,
-    comment: 'Role of the user (e.g., developer, product owner)',
-  },
-  createdAt: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW,
-  },
-  updatedAt: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW,
-  },
-}, {
-  tableName: 'User' // Explicitly set table name to 'User'
-});
+    role: {
+      type: DataTypes.ENUM('developer', 'product owner', 'tester', 'teammate', 'scrum master', 'administrator'),
+      allowNull: false,
+      comment: 'Role of the user (e.g., developer, product owner)',
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  }, {
+    tableName: 'User' // Explicitly set table name to 'User'
+  });
 
-User.beforeCreate(async (user) => {
-  const salt = await bcrypt.genSalt(10);
-  user.password = await bcrypt.hash(user.password, salt);
-});
+  User.beforeCreate(async (user) => {
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+  });
 
-User.prototype.comparePassword = function(candidatePassword) {
-  return bcrypt.compare(candidatePassword, this.password);
+  User.prototype.comparePassword = function(candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+  };
+
+  return User;
 };
 
-module.exports = User;
+module.exports = UserDefinition;
--- src/models/userStory.js
+++ src/models/userStory.js
@@ -1,58 +1,68 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
-const UserStory = sequelize.define('UserStory', {
-    id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-    },
-    action: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        comment: 'What the user wants to do',
-    },
-    need: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        comment: 'Reason or benefit for the action',
-    },
-    status: {
-        type: DataTypes.ENUM('todo', 'doing', 'done'),
-        allowNull: false,
-        defaultValue: 'todo',
-        comment: 'Current status of the User Story',
-    },
-    priority: {
-        type: DataTypes.ENUM('low', 'medium', 'high'),
-        allowNull: false,
-        defaultValue: 'medium',
-        comment: 'Priority level of the User Story',
-    },
-    createdAt: {
-        type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW,
-    },
-    updatedAt: {
-        type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW,
-    },
-    assignedTo: { // Explicitly define assignedTo as a column
-        type: DataTypes.INTEGER,
-            allowNull: true, // Or false, depending on requirements
+const UserStoryDefinition = (sequelize) => {
+    const UserStory = sequelize.define('UserStory', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        action: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            comment: 'What the user wants to do',
+        },
+        need: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            comment: 'Reason or benefit for the action',
+        },
+        status: {
+            type: DataTypes.ENUM('todo', 'doing', 'done'),
+            allowNull: false,
+            defaultValue: 'todo',
+            comment: 'Current status of the User Story',
+        },
+        priority: {
+            type: DataTypes.ENUM('low', 'medium', 'high'),
+            allowNull: false,
+            defaultValue: 'medium',
+            comment: 'Priority level of the User Story',
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW,
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW,
+        },
+        role: {
+            type: DataTypes.STRING,
+            allowNull: true, // Role is optional
+            comment: 'Role of the user related to the user story',
+        },
+        assignedTo: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
             references: {
-              model: 'User', // Name of the User model
-              key: 'id',       // Primary key of the User model
-            },
-            onDelete: 'SET NULL', // Or 'CASCADE', 'RESTRICT', etc.
-            onUpdate: 'CASCADE',
-          },
-}, {
-    tableName: 'UserStories',
-    comment: 'Represents a User Story in the Agile workflow',
-});
+                  model: 'User',
+                  key: 'id',
+                },
+                onDelete: 'SET NULL',
+                onUpdate: 'CASCADE',
+        },
+    }, {
+        tableName: 'UserStories',
+        comment: 'Represents a User Story in the Agile workflow',
+    });
 
+    return UserStory;
+};
 
+module.exports = (sequelize) => {
+  const UserStory = UserStoryDefinition(sequelize);
 
-
-module.exports = UserStory;
+  return UserStory; // Return the UserStory model without associations
+};
