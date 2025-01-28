bcbf861cf39c6bfb1bad80c122a35ac42c56631f
Kody <157696416+kydoCode@users.noreply.github.com>
2025-01-28 12:18:24 +0100

    Fixed US display

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,8 +1,15 @@
 const errorHandler = (err, req, res, next) => {
-    console.error("Backend error:", err); // Log the entire error object
-    console.error("Backend error:", err);
-    res.status(500).json({ message: 'Une erreur est survenue sur le serveur', error: err.message, stack: err.stack, fullError: err });
+    console.error("Backend error:", JSON.stringify(err, null, 2));
+    let errorMessage = 'Une erreur est survenue sur le serveur';
+    let errorDetails = {};
+
+    if (process.env.NODE_ENV !== 'production') {
+        errorMessage = err.message;
+        errorDetails = { stack: err.stack, fullError: err };
+    }
+
+    res.status(500).json({ message: errorMessage, error: err.message, ...errorDetails });
     next(err);
-  };
+};
 
 module.exports = errorHandler;
